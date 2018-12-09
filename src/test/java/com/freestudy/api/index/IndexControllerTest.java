ce578e80bc64be2fb089a2eac65b5609433e5ef0
holdonnn <rururu0729@gmail.com>
2018-12-10 00:36:57 +0900

    Change IndexControllerTest into integration test

--- src/test/java/com/freestudy/api/index/IndexControllerTest.java
+++ src/test/java/com/freestudy/api/index/IndexControllerTest.java
@@ -4,7 +4,8 @@ package com.freestudy.api.index;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -14,7 +15,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest
+@SpringBootTest
+@AutoConfigureMockMvc
 public class IndexControllerTest {
 
 
