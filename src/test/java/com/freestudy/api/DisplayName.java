00c629945cf1774bfa9f9fa8291c8e1d6dc68421
holdonnn <rururu0729@gmail.com>
2018-12-15 21:05:41 +0900

    Add test infra classes

---
+++ src/test/java/com/freestudy/api/DisplayName.java
@@ -0,0 +1,16 @@
+package com.freestudy.api;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.SOURCE)
+public @interface DisplayName {
+  /**
+   * https://junit.org/junit5/docs/current/user-guide/#writing-tests-display-names
+   * junit 5에 올리면 이 에노테이션을 junit5 의 DisplayName 으로 변경합니다.
+   */
+  String value();
+}
