341bc498063f67f567eb1aeec6d3b5306d56cdf7
holdonnn <rururu0729@gmail.com>
2018-12-15 21:05:08 +0900

    Clean import

--- src/test/java/com/freestudy/api/event/EventControllerTest.java
+++ src/test/java/com/freestudy/api/event/EventControllerTest.java
@@ -10,7 +10,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
