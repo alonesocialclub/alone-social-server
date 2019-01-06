c9bca71b0a6b3e9571830d61938e1dec77eab7bf
holdonnn <rururu0729@gmail.com>
2019-01-07 00:42:50 +0900

    Remove unused codes

--- src/main/java/com/freestudy/api/config/AppConfig.java
+++ src/main/java/com/freestudy/api/config/AppConfig.java
@@ -1,47 +1,37 @@
 package com.freestudy.api.config;
 
 
-import com.freestudy.api.account.Account;
-import com.freestudy.api.account.AccountRole;
-import com.freestudy.api.account.AccountService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.core.env.Environment;
 
-import java.util.Set;
+import java.util.Arrays;
 
 @Configuration
 public class AppConfig {
-
-  @Autowired
-  private AccountService accountService;
 
   @Bean
   public ModelMapper modelMapper() {
     return new ModelMapper();
   }
 
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-    return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-  }
+  @Autowired
+  public AppProperties appProperties;
 
+  @Autowired
+  private Environment environment;
 
   @Bean
   public ApplicationRunner applicationRunner() {
     return new ApplicationRunner() {
       @Override
       public void run(ApplicationArguments args) throws Exception {
-        Account account = Account
-                .builder()
-                .email("jh@email.com").password("1234").roles(Set.of(AccountRole.ADMIN, AccountRole.USER))
-                .build();
-        accountService.saveAccount(account);
+        System.out.println("========================================");
+        System.out.println("========================================");
       }
     };
   }
