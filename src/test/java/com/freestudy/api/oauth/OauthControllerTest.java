f0e60f3872cde40954467879cf228daf0dd519e2
holdonnn <rururu0729@gmail.com>
2019-01-13 00:28:51 +0900

    페이스북 인증 api 문서화

--- src/test/java/com/freestudy/api/oauth/OauthControllerTest.java
+++ src/test/java/com/freestudy/api/oauth/OauthControllerTest.java
@@ -3,6 +3,9 @@ package com.freestudy.api.oauth;
 import com.freestudy.api.BaseControllerTest;
 import org.junit.Test;
 
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrlPattern;
@@ -13,10 +16,22 @@ public class OauthControllerTest extends BaseControllerTest {
   @Test
   public void testOauth2AuthorizeFacebook() throws Exception {
 
-    var result = mockMvc.perform(get("/oauth2/authorize/facebook"));
+    var perform = mockMvc.perform(
+            get("/oauth2/authorize/facebook")
+                    .param("redirect_uri", "https://myweb.com")
+    );
 
-    result.andDo(print());
-    result.andExpect(status().is3xxRedirection());
-    result.andExpect(redirectedUrlPattern("https://www.facebook.com/**"));
+    perform.andDo(print())
+            .andExpect(status().is3xxRedirection())
+            .andExpect(redirectedUrlPattern("https://www.facebook.com/**"));
+
+    perform.andDo(
+            document(
+                    "oauth2-facebook",
+                    requestParameters(
+                            parameterWithName("redirect_uri").description("인증 후 리다이렉트 될 url이며 화이트리스트 기반으로 관리된다.")
+                    )
+            )
+    );
   }
 }
