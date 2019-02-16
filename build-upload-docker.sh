6dacacc552cf35ef653c51ba086654c6af9e1770
holdonnn <rururu0729@gmail.com>
2019-02-16 15:09:31 +0900

    Update docker registry url

--- build-upload-docker.sh
+++ build-upload-docker.sh
@@ -1,4 +1,4 @@
 ./gradlew clean bootJar
-$(aws ecr get-login --no-include-email --region ap-northeast-1)
+$(aws ecr get-login --no-include-email --region ap-northeast-2)
 docker-compose build --no-cache api
 docker-compose push api
