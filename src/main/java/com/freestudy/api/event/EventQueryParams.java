3adb4cf1923f7ff3c929d9bd89a1f865cae79e6a
holdonnn <rururu0729@gmail.com>
2019-03-10 10:59:09 +0900

    Add Coordinate class

--- src/main/java/com/freestudy/api/event/EventQueryParams.java
+++ src/main/java/com/freestudy/api/event/EventQueryParams.java
@@ -1,7 +1,9 @@
 package com.freestudy.api.event;
 
+import com.freestudy.api.event.type.Coordinate;
 import com.freestudy.api.event.type.EventQueryType;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Optional;
 
@@ -9,7 +11,19 @@ import java.util.Optional;
 public class EventQueryParams {
   private Optional<EventQueryType> type;
 
+  @Setter
+  private Double longitude;
+  @Setter
+  private Double latitude;
+
   public void setType(final EventQueryType type) {
     this.type = Optional.ofNullable(type);
+  }
+
+  public Optional<Coordinate> getCoordinate() {
+    if (this.latitude != null && this.longitude != null) {
+      return Optional.of(new Coordinate(longitude, latitude));
+    }
+    return Optional.empty();
   }
 }
