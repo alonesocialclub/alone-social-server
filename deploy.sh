a5930e728540ac5063e2ce240a60956348df45d3
holdonnn <rururu0729@gmail.com>
2019-03-17 16:43:40 +0900

    clean before docker

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 set -e
-./gradlew docker dockerPush
+./gradlew clean dockerPrepare dockerPush
 $(aws ecr get-login --no-include-email --region ap-northeast-2)
 ssh -i server.pem ec2-user@13.125.58.128 './update.sh'
