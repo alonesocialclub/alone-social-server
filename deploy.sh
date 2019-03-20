2b0eb7e4e65980696f9cbea92a654c82c801e869
holdonnn <rururu0729@gmail.com>
2019-03-21 00:04:13 +0900

    Authorization before docker push

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 set -e
-./gradlew clean dockerPrepare dockerPush
 $(aws ecr get-login --no-include-email --region ap-northeast-2)
+./gradlew clean dockerPrepare dockerPush
 ssh -i server.pem ec2-user@13.125.58.128 './update.sh'
