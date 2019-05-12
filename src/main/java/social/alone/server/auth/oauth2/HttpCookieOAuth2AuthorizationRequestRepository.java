6afd57e1adfc30d92afe4a176bb278876df3368b
holdonnn <rururu0729@gmail.com>
2019-05-12 19:30:07 +0900

    private package

--- src/main/java/social/alone/server/auth/oauth2/HttpCookieOAuth2AuthorizationRequestRepository.java
+++ src/main/java/social/alone/server/auth/oauth2/HttpCookieOAuth2AuthorizationRequestRepository.java
@@ -42,7 +42,7 @@ public class HttpCookieOAuth2AuthorizationRequestRepository implements Authoriza
     return this.loadAuthorizationRequest(request);
   }
 
-  public void removeAuthorizationRequestCookies(HttpServletRequest request, HttpServletResponse response) {
+  void removeAuthorizationRequestCookies(HttpServletRequest request, HttpServletResponse response) {
     CookieUtils.deleteCookie(request, response, OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME);
     CookieUtils.deleteCookie(request, response, REDIRECT_URI_PARAM_COOKIE_NAME);
   }
