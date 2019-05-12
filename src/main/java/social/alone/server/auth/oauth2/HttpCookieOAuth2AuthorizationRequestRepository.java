0242f02440c855e7dd0e832655d905940f0f2b81
holdonnn <rururu0729@gmail.com>
2019-05-12 19:28:22 +0900

    Make private package

--- src/main/java/social/alone/server/auth/oauth2/HttpCookieOAuth2AuthorizationRequestRepository.java
+++ src/main/java/social/alone/server/auth/oauth2/HttpCookieOAuth2AuthorizationRequestRepository.java
@@ -11,8 +11,8 @@ import javax.servlet.http.HttpServletResponse;
 
 @Component
 public class HttpCookieOAuth2AuthorizationRequestRepository implements AuthorizationRequestRepository<OAuth2AuthorizationRequest> {
-  public static final String OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME = "oauth2_auth_request";
-  public static final String REDIRECT_URI_PARAM_COOKIE_NAME = "redirect_uri";
+  private static final String OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME = "oauth2_auth_request";
+  static final String REDIRECT_URI_PARAM_COOKIE_NAME = "redirect_uri";
   private static final int cookieExpireSeconds = 180;
 
   @Override
