fd2086b00eebf73a457d1cd8751629c5b56afed1
holdonnn <rururu0729@gmail.com>
2019-05-26 23:31:03 +0900

    Disabled firebaseconfig for test

--- src/main/java/social/alone/server/common/config/FirebaseConfig.java
+++ src/main/java/social/alone/server/common/config/FirebaseConfig.java
@@ -4,12 +4,14 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.io.InputStream;
 
 @Configuration
+@Profile("!test")
 public class FirebaseConfig {
 
     @PostConstruct
