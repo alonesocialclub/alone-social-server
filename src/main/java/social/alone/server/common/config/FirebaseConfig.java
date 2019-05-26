8a4a80caed7867abbc0981f7b301d0dc884182ee
holdonnn <rururu0729@gmail.com>
2019-05-26 23:02:32 +0900

    Trim FirebaseConfig

--- src/main/java/social/alone/server/common/config/FirebaseConfig.java
+++ src/main/java/social/alone/server/common/config/FirebaseConfig.java
@@ -6,26 +6,26 @@ import com.google.firebase.FirebaseOptions;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
 
     @PostConstruct
-    public void firebaseAppInit() throws IOException {
-        try{
-            FileInputStream serviceAccount = new FileInputStream("alone-social-club-firebase-adminsdk.json");
-            FirebaseOptions options = new FirebaseOptions.Builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .build();
+    public void init() throws IOException {
+        var path = "alone-social-club-firebase-adminsdk.json";
+        ClassLoader classLoader = getClass().getClassLoader();
+        InputStream serviceAccount = classLoader.getResourceAsStream(path);
 
-            if(FirebaseApp.getApps().isEmpty()){
-                FirebaseApp.initializeApp(options);
-            }
-        } catch (FileNotFoundException ignored){
-            return;
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        if (FirebaseApp.getApps().isEmpty()) {
+            FirebaseApp.initializeApp(options);
         }
     }
+
 }
