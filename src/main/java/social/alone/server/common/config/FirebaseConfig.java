febbe0f45d46c3e05a1358f074157756a2c86cfa
holdonnn <rururu0729@gmail.com>
2019-05-26 19:03:36 +0900

    Add firebase admin for server side push

---
+++ src/main/java/social/alone/server/common/config/FirebaseConfig.java
@@ -0,0 +1,25 @@
+package social.alone.server.common.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Configuration
+public class FirebaseConfig {
+
+    @PostConstruct
+    public void firebaseAppInit() throws IOException {
+        FileInputStream serviceAccount = new FileInputStream("alone-social-club-firebase-adminsdk.json");
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        FirebaseApp.initializeApp(options);
+    }
+}
