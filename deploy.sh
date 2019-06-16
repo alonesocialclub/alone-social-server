e5eb3174fcafa9bf77d665b92a4a286845106d1e
holdonnn <rururu0729@gmail.com>
2019-06-16 19:01:05 +0900

    serverip

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 set -e
 $(aws ecr get-login --no-include-email --region ap-northeast-2)
 ./gradlew clean dockerPrepare dockerPush
-ssh -i server.pem ec2-user@13.125.58.128 './update.sh'
+ssh -i server.pem ec2-user@52.78.145.107 './update.sh'
