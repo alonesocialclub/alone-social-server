f2e359ec213509a16805967ec426b5a6415f9557
holdonnn <rururu0729@gmail.com>
2019-07-06 23:44:50 +0900

    Trim

--- src/main/kotlin/social/alone/server/common/SentryConfig.kt
+++ src/main/kotlin/social/alone/server/common/SentryConfig.kt
@@ -1,20 +1,17 @@
 package social.alone.server.common
 
 import io.sentry.Sentry
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import org.springframework.web.servlet.HandlerExceptionResolver
-import social.alone.server.common.config.AppProperties
 
 
 @Configuration
 @ConfigurationProperties(prefix = "app.sentry")
 class SentryConfig {
 
-    @Autowired
     lateinit var dsn: String
 
     @Bean
