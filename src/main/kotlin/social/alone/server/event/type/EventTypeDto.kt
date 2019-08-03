a2d5f2ce706bf72569275671eefa1175ba33240c
holdonnn <rururu0729@gmail.com>
2019-08-03 22:28:40 +0900

    Fix create event test

--- src/main/kotlin/social/alone/server/event/type/EventTypeDto.kt
+++ src/main/kotlin/social/alone/server/event/type/EventTypeDto.kt
@@ -1,17 +1,7 @@
 package social.alone.server.event.type
 
-import lombok.*
-
 import javax.validation.constraints.Size
 
-@Getter
-@AllArgsConstructor
-@NoArgsConstructor
-@EqualsAndHashCode(of = ["value"])
-class EventTypeDto(id: Long?, value: String) {
+data class EventTypeDto(val id: Long? = null, @Size(min = 1, max = 30) val value: String) {
 
-    var id: Long? = null
-
-    @Size(min = 1, max = 30)
-    var value: String? = null
 }
