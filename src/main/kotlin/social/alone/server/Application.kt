2d48e04f8489dfb7814c4e52019eaf644c5613dd
holdonnn <rururu0729@gmail.com>
2019-08-11 16:09:10 +0900

    Set timezone

--- src/main/kotlin/social/alone/server/Application.kt
+++ src/main/kotlin/social/alone/server/Application.kt
@@ -3,14 +3,27 @@ package social.alone.server
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import java.time.LocalDateTime
+import java.util.TimeZone
+import javax.annotation.PostConstruct
 
 
 @SpringBootApplication
 
 open class Application {
     companion object {
+
+        @PostConstruct
+        fun started() {
+            TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"))
+        }
+
+
         @JvmStatic
         fun main(args: Array<String>) {
+            println("+=============")
+            println(LocalDateTime.now())
+            println("+=============")
             SpringApplication.run(Application::class.java, *args)
         }
     }
