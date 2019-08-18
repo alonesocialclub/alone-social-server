f899734afc32abdca40f6df8d7ce3d2e938573f6
holdonnn <rururu0729@gmail.com>
2019-08-18 12:56:01 +0900

    No semi

--- src/test/kotlin/social/alone/server/event/EventTest.kt
+++ src/test/kotlin/social/alone/server/event/EventTest.kt
@@ -1,11 +1,11 @@
-package social.alone.server.event;
+package social.alone.server.event
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.Test;
-import social.alone.server.event.domain.Event;
-import social.alone.server.event.dto.EventDto;
-import social.alone.server.location.LocationDto;
-import social.alone.server.user.domain.User;
+import org.junit.Test
+import social.alone.server.event.domain.Event
+import social.alone.server.event.dto.EventDto
+import social.alone.server.location.LocationDto
+import social.alone.server.user.domain.User
 import java.time.LocalDateTime
 
 
@@ -13,13 +13,13 @@ class EventTest {
 
     @Test
     fun builderTest() {
-        val user = User("test@test.com", "1234", "1234");
-        val location = LocationDto("남부순환로", "스타벅스", 123.123, 123.123, "https://naver.com");
+        val user = User("test@test.com", "1234", "1234")
+        val location = LocationDto("남부순환로", "스타벅스", 123.123, 123.123, "https://naver.com")
         val eventDto = EventDto(
                 "name", "description", location, LocalDateTime.now(), LocalDateTime.now().plusHours(3)
         )
         val event = Event(eventDto, user)
-        assertThat(event).isNotNull;
+        assertThat(event).isNotNull
     }
 
 }
