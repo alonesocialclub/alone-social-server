e6eef1de00238a89650fa606c8f302e5d2c5b24f
holdonnn <rururu0729@gmail.com>
2019-08-24 14:29:58 +0900

    Add UserView

---
+++ src/main/kotlin/social/alone/server/user/domain/UserView.kt
@@ -0,0 +1,20 @@
+package social.alone.server.user.domain
+
+import social.alone.server.interest.Interest
+
+
+data class UserView(
+        val id: Long?,
+        val name: String,
+        val imageUrl: String,
+        val interests: MutableSet<Interest>
+)
+
+val DEFAULT_PROFILE = "https://alone-social-static-image.s3.ap-northeast-2.amazonaws.com/profile.png";
+
+fun User.view() = UserView(
+        id = id,
+        name = name.orEmpty(),
+        imageUrl = imageUrl ?: DEFAULT_PROFILE,
+        interests = interests
+)
---
+++ src/test/java/social/alone/server/user/domain/UserViewKtTest.kt
@@ -0,0 +1,17 @@
+package social.alone.server.user.domain
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+
+
+class UserViewKtTest {
+
+    @Test
+    fun test_image_default_url() {
+        val user: User = User("email@email.com", "password", "name");
+
+        assertThat(user.view()).isNotNull
+        assertThat(user.view().imageUrl).isNotEmpty()
+
+    }
+}
