b5cdb953a908716c631703e056bc9a3df4c61c93
holdonnn <rururu0729@gmail.com>
2019-10-07 01:19:28 +0900

    Add testcase

--- src/main/kotlin/social/alone/server/image/ImageController.kt
+++ src/main/kotlin/social/alone/server/image/ImageController.kt
@@ -1,14 +1,13 @@
 package social.alone.server.image
 
 import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestPart
-import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
-import social.alone.server.infrastructure.S3Uploader
 
 
-@RestController
+@Controller
 class ImageController(private val imageUploader: ImageUploader) {
 
     @PostMapping("/images")
