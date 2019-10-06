767417e52a59c23d9d2425c9584a75376cbb47bd
holdonnn <rururu0729@gmail.com>
2019-10-07 01:43:05 +0900

    Implement anonymous join API

---
+++ src/main/kotlin/social/alone/server/auth/anonymous/JoinController.kt
@@ -0,0 +1,26 @@
+package social.alone.server.auth.anonymous
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+import social.alone.server.auth.AuthTokenGenerator
+import social.alone.server.user.domain.UserTokenView
+import social.alone.server.user.repository.UserRepository
+import social.alone.server.user.service.UserEnrollService
+import javax.validation.Valid
+
+@RestController
+class AnonymousJoinController(
+        private val userRepository: UserRepository, private val userEnrollService: UserEnrollService,
+        private val authTokenGenerator: AuthTokenGenerator
+) {
+
+    @PostMapping("/anonymous/join")
+    fun anonymousJoin(@RequestBody @Valid request: JoinByProfileRequest): ResponseEntity<*> {
+        val user = userEnrollService.byProfile(request)
+        val token = authTokenGenerator.byUser(user)
+        val userResource = UserTokenView(user, token)
+        return ResponseEntity.ok(userResource)
+    }
+}
--- src/main/kotlin/social/alone/server/user/service/UserEnrollService.kt
+++ src/main/kotlin/social/alone/server/user/service/UserEnrollService.kt
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.util.Assert
 import social.alone.server.auth.FacebookUserInfoFetcher
+import social.alone.server.auth.anonymous.JoinByProfileRequest
 import social.alone.server.auth.oauth2.user.FacebookOAuth2UserInfo
 import social.alone.server.infrastructure.S3Uploader
 import social.alone.server.user.domain.AuthProvider
@@ -28,18 +29,21 @@ class UserEnrollService(private val userRepository: UserRepository, private val 
     }
 
     fun byFacebook(accessToken: String): User {
-        val userInfo = facebookUserInfoFetcher!!.getUserInfo(accessToken)
-        //        userInfo.setS3ImageUrl(
-        //                s3Uploader.upload(userInfo.getId() + "_fb_profile.jpeg", userInfo.getImageUrl())
-        //        );
+        val userInfo = facebookUserInfoFetcher.getUserInfo(accessToken)
         return this.enrollByFacebook(userInfo)
     }
 
     private fun enrollByFacebook(
             userInfo: FacebookOAuth2UserInfo): User {
         val email = userInfo.email
-        val byEmail = userRepository!!.findByEmailAndProvider(email, AuthProvider.facebook)
+        val byEmail = userRepository.findByEmailAndProvider(email, AuthProvider.facebook)
         return byEmail.orElseGet { userRepository.save(User(userInfo, AuthProvider.facebook)) }
+    }
+
+    fun byProfile(request: JoinByProfileRequest): User {
+        val fakeEmail = request.profile.name + "@anonymous.com"
+        val byEmail = userRepository.findByEmailAndProvider(fakeEmail, AuthProvider.facebook)
+        return byEmail.orElseGet { userRepository.save(User(fakeEmail, null, request.profile.name)) }
     }
 
 }
