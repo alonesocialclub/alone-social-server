50ad7f3b238a595a9f1d750a43d0e0fdb2cf9d25
holdonnn <rururu0729@gmail.com>
2019-10-09 12:11:17 +0900

    Add AuthoConfigurationRestDocs

--- src/test/kotlin/social/alone/server/image/ImageControllerTest.kt
+++ src/test/kotlin/social/alone/server/image/ImageControllerTest.kt
@@ -5,8 +5,10 @@ import org.junit.runner.RunWith
 import org.mockito.BDDMockito.given
 import org.mockito.Mockito
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.context.annotation.Import
 import org.springframework.mock.web.MockMultipartFile
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation
 import org.springframework.test.context.junit4.SpringRunner
@@ -14,11 +16,14 @@ import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import org.springframework.web.multipart.MultipartFile
+import social.alone.server.RestDocsConfiguration
 import java.io.FileInputStream
 
 
 @RunWith(SpringRunner::class)
 @WebMvcTest(ImageController::class, secure = false)
+@AutoConfigureRestDocs(uriScheme = "https", uriHost = "server.money-whip.com", uriPort = 443)
+@Import(RestDocsConfiguration::class)
 class ImageControllerTest(
 
 ) {
