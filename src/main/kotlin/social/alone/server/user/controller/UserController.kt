46c7212aeb30a1577efd2c404e724833aab72a28
holdonnn <rururu0729@gmail.com>
2019-10-12 16:08:41 +0900

    Implement User READ API

--- src/main/kotlin/social/alone/server/user/controller/UserController.kt
+++ src/main/kotlin/social/alone/server/user/controller/UserController.kt
@@ -1,54 +1,19 @@
 package social.alone.server.user.controller
 
-import org.springframework.http.ResponseEntity
-import org.springframework.security.access.prepost.PreAuthorize
-import org.springframework.stereotype.Controller
-import org.springframework.validation.Errors
-import org.springframework.web.bind.annotation.*
-import social.alone.server.auth.oauth2.user.CurrentUser
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
 import social.alone.server.controller.BaseController
 import social.alone.server.user.domain.User
-import social.alone.server.user.domain.UserTokenView
-import social.alone.server.user.dto.UserDto
-import social.alone.server.user.service.CustomUserDetailService
-import javax.validation.Valid
+import social.alone.server.user.domain.UserView
+import social.alone.server.user.domain.view
 
-//@Controller
-//@RequestMapping(value = ["/api/users"])
-//class UserController (val userService: CustomUserDetailService): BaseController() {
-//
-//    @GetMapping("/me")
-//    @PreAuthorize("hasRole('USER')")
-//    fun getUsersMe(@CurrentUser user: User): ResponseEntity<*> {
-//        return buildResponse(user)
-//    }
-//
-//    @PutMapping("/me")
-//    @PreAuthorize("hasRole('USER')")
-//    fun postUsersMe(
-//            @CurrentUser user: User,
-//            @Valid @RequestBody userDto: UserDto,
-//            errors: Errors
-//    ): ResponseEntity<*> {
-//
-//        if (errors.hasErrors()) {
-//            return BadRequest(errors)
-//        }
-//
-//        val updatedUser = userService.update(user, userDto)
-//
-//        return buildResponse(updatedUser)
-//    }
-//
-//    @GetMapping("/{id}")
-//    fun getUser(@PathVariable("id") user: User): ResponseEntity<*> {
-//        return buildResponse(user)
-//    }
-//
-//    private fun buildResponse(user: User): ResponseEntity<*> {
-//        val userResource = UserTokenView(user)
-//        return ResponseEntity.ok(userResource)
-//    }
-//
-//
-//}
+@RestController
+class UserController : BaseController() {
+
+    @GetMapping("/users/{id}")
+    fun getUser(@PathVariable("id") user: User): UserView {
+        return user.view()
+    }
+
+}
