1b49bf52b428ae331f862eead6e463e650ad48f5
holdonnn <rururu0729@gmail.com>
2019-10-12 15:48:37 +0900

    Implement User enrollment with Image

--- src/main/kotlin/social/alone/server/user/service/UserEnrollService.kt
+++ src/main/kotlin/social/alone/server/user/service/UserEnrollService.kt
@@ -7,15 +7,23 @@ import org.springframework.util.Assert
 import social.alone.server.auth.FacebookUserInfoFetcher
 import social.alone.server.auth.anonymous.JoinByProfileRequest
 import social.alone.server.auth.oauth2.user.FacebookOAuth2UserInfo
+import social.alone.server.image.Image
+import social.alone.server.image.ImageRepository
 import social.alone.server.infrastructure.S3Uploader
 import social.alone.server.user.domain.AuthProvider
 import social.alone.server.user.domain.Profile
 import social.alone.server.user.domain.User
 import social.alone.server.user.repository.UserRepository
+import java.util.*
 
 @Service
 @Transactional
-class UserEnrollService(private val userRepository: UserRepository, private val passwordEncoder: PasswordEncoder, private val facebookUserInfoFetcher: FacebookUserInfoFetcher, private val s3Uploader: S3Uploader) {
+class UserEnrollService(
+        private val userRepository: UserRepository,
+        private val passwordEncoder: PasswordEncoder,
+        private val facebookUserInfoFetcher: FacebookUserInfoFetcher,
+        private val imageRepository: ImageRepository
+) {
 
     fun enrollByEmailPassword(
             email: String, password: String, name: String
@@ -48,9 +56,15 @@ class UserEnrollService(private val userRepository: UserRepository, private val 
             return byEmail.get()
         }
 
-        val profile = Profile(request.profile.name)
+        val profile = requestToProfile(request)
         val user = User(fakeEmail, null, profile)
         return userRepository.save(user)
+    }
+
+
+    private fun requestToProfile(request: JoinByProfileRequest): Profile {
+        val image = request.profile.image?.id?.let { imageRepository.findById(it) } ?: Optional.empty()
+        return Profile(request.profile.name, image.orElse(null))
     }
 
 }
