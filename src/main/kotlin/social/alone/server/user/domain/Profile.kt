5e53ee0a10ba6fc7267e5b3f7b4c59174896c6b1
holdonnn <rururu0729@gmail.com>
2019-10-12 15:39:57 +0900

    Add Profile Image mapping

--- src/main/kotlin/social/alone/server/user/domain/Profile.kt
+++ src/main/kotlin/social/alone/server/user/domain/Profile.kt
@@ -1,16 +1,24 @@
 package social.alone.server.user.domain
 
 import org.jetbrains.annotations.NotNull
+import social.alone.server.image.Image
 import javax.persistence.Embeddable
+import javax.persistence.OneToOne
 import javax.validation.constraints.Size
 
 
 @Embeddable
 data class Profile(
         @NotNull
-        @Size(max = 20) var name: String = ""
+        @Size(max = 20) var name: String = "",
+        @OneToOne
+        var image: Image? = null
 ) {
     fun updateName(name: String) {
         this.name = name
+    }
+
+    fun updateImage(image:Image) {
+        this.image = image
     }
 }
