c80d8786b64649709938ecce938f0ecc97863198
holdonnn <rururu0729@gmail.com>
2019-10-12 16:10:28 +0900

    Move /me controller method

--- src/main/kotlin/social/alone/server/user/controller/UserController.kt
+++ src/main/kotlin/social/alone/server/user/controller/UserController.kt
@@ -1,8 +1,10 @@
 package social.alone.server.user.controller
 
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
+import social.alone.server.auth.oauth2.user.CurrentUser
 import social.alone.server.controller.BaseController
 import social.alone.server.user.domain.User
 import social.alone.server.user.domain.UserView
@@ -13,6 +15,12 @@ class UserController : BaseController() {
 
     @GetMapping("/users/{id}")
     fun getUser(@PathVariable("id") user: User): UserView {
+        return user.view()
+    }
+
+    @GetMapping("/me")
+    @PreAuthorize("hasRole('USER')")
+    fun getUsersMe(@CurrentUser user: User): UserView {
         return user.view()
     }
 
