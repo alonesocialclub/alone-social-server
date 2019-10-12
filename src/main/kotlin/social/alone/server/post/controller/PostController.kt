9c61d0893a1395db6fa2f7638d6322c545ce6413
holdonnn <rururu0729@gmail.com>
2019-10-12 21:44:58 +0900

    Implement Post delete API

--- src/main/kotlin/social/alone/server/post/controller/PostController.kt
+++ src/main/kotlin/social/alone/server/post/controller/PostController.kt
@@ -7,13 +7,15 @@ import social.alone.server.auth.oauth2.user.CurrentUser
 import social.alone.server.controller.BaseController
 import social.alone.server.post.domain.Post
 import social.alone.server.post.dto.PostCreateRequest
+import social.alone.server.post.repository.PostRepository
 import social.alone.server.post.service.PostCreateService
 import social.alone.server.user.domain.User
 
 @Controller
 @RequestMapping(value = ["/posts"])
 class PostController(
-        val postCreateService: PostCreateService
+        val postCreateService: PostCreateService,
+        val postRepository: PostRepository
 ) : BaseController() {
 
     @GetMapping("/{id}")
@@ -30,6 +32,14 @@ class PostController(
     ): ResponseEntity<*> {
         val post = postCreateService.create(user, postCreateRequest)
         return ResponseEntity.ok(post)
+    }
+
+    @DeleteMapping("/{id}")
+    fun deletePost(
+            @PathVariable("id") post: Post
+    ): ResponseEntity<*> {
+        postRepository.delete(post)
+        return ResponseEntity.noContent().build<Any>()
     }
 
 }
