21c67b76a26fc9959fa1b3270bf2971e090345eb
holdonnn <rururu0729@gmail.com>
2019-10-13 15:37:31 +0900

    Add profile name validation of size

--- src/main/kotlin/social/alone/server/auth/anonymous/ProfileRequest.kt
+++ src/main/kotlin/social/alone/server/auth/anonymous/ProfileRequest.kt
@@ -2,10 +2,12 @@ package social.alone.server.auth.anonymous
 
 import social.alone.server.pickture.PictureRequest
 import javax.validation.constraints.NotEmpty
+import javax.validation.constraints.Size
 
 
 data class ProfileRequest(
         @field:NotEmpty(message = "Name is required")
+        @field:Size(max = 16, message = "Name should be 1~16 characters")
         val name: String,
         val picture: PictureRequest? = null
 )
