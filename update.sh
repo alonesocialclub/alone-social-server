ef74384eff3d543bba64a121c41c150ab0add85c
holdonnn <rururu0729@gmail.com>
2019-10-13 19:39:59 +0900

    Remove update.sh

--- update.sh
+++
@@ -1,6 +0,0 @@
-URL="459648971904.dkr.ecr.ap-northeast-2.amazonaws.com/alone-social-server"
-docker rm $(docker stop $(docker ps -a -q --filter ancestor=$URL --format="{{.ID}}"))
-docker rmi $URL
-$(aws ecr get-login --no-include-email --region ap-northeast-2)
-docker pull $URL
-docker run -p 80:8080 -d $URL
