02586d88e6c3267aa468c33eeb89d3d9040ed966
김동현 <kdhfred96@gmail.com>
2019-10-13 15:09:02 +0900

    Add ImageDownlaoder objects covert to image io

---
+++ src/main/kotlin/social/alone/server/pickture/ImageDownloader.kt
@@ -0,0 +1,16 @@
+package social.alone.server.pickture
+
+import org.springframework.stereotype.Service
+import social.alone.server.infrastructure.S3Downloader
+import java.awt.image.BufferedImage
+import javax.imageio.ImageIO
+
+
+@Service
+class ImageDownloader(private val s3Downloader: S3Downloader) {
+    fun download(imageName: String): BufferedImage{
+        val imageObj = s3Downloader.download(imageName)
+        val bufferedImage = ImageIO.read(imageObj.objectContent)
+        return bufferedImage
+    }
+}
